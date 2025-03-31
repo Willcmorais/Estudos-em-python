3235cd6aea4d8007f9efb34a8a94fab075b6de08
William Coelho <willcmorais@gmail.com>
2025-03-31 17:54:00 -0300

    commit desafio manip dados 31-03-25-tarde

--- desafio-txt-manip.py
+++ desafio-txt-manip.py
@@ -1,4 +1,7 @@
+# Analisa o nome, transforma para maiúsculo e minúsculo, conta quantas letras tem o nome sem espaços e diz o primeiro nome com qnt de letras
+# variável que recebe um input str.
 name = str(input('Digite seu nome completo: ').strip())  # tira os espaços que podem aparecer antes ou depois do nome
+
 print('Analisando nome........')
 print('O nome transformado para maiúsculo: {}'.format(name.upper()))
 print('O nome transformado para minúsculo: {}'.format(name.lower()))
@@ -6,6 +9,28 @@ print('O nome transformado para minúsculo: {}'.format(name.lower()))
 print('Seu nome tem {} letras'.format(len(name) - name.count(' ')))
 # vai pegar quantos caracteres leva até achar o primeiro espaço e vai mostrar qual posição ele está
 # print('Seu primeiro nome tem {} letras'.format(name.find(' ')))
-separa = name.split()  # separa vai receber o nome separado pos listas, o primeiro nome inicia no 0
+separa = name.split()  # separa vai receber o nome separado das listas, o primeiro nome inicia no 0
 # vai mostrar meu primeiro nome que está localizado na posição da lista 0 e a quantidade de espaços que ele tem
 print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
+
+# vai dizer qual número está na unidade, na dezena, na centena e na unidade a partir de uma fórmula matemática
+# num = str(input('Digite um número: '))
+num = int(input('Digite um número: '))
+u = num % 10  # vai pegar o número, dividir por 10, e o resto da divisão será a unidade
+d = num // 10 % 10  # vai pegar o número dividir por 10, pegar o resto inteiro da divisão e dividir por 10 novamente
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Analisando número.......')
+# print('O número presente na casa das unidades é o {}'.format(num[3]))
+# print('O número presente na casa das dezenas é o {}'.format(num[2]))
+# print('O número presente na casa das centenas é o {}'.format(num[1]))
+# print('O número presente na casa do milhar é o {}'.format(num[0]))
+print('UNIDADE: {}'.format(u))
+print('DEZENA: {}'.format(d))
+print('CENTENA: {}'.format(c))
+print('MILHAR: {}'.format(m))
+
+# Vai pegar o nome da minha cidade, transformar para apenas primeira letra em maiúsculo e vai perguntar se tem esse nome nela, se tiver é tru
+city = input('Informe qual é a sua cidade: ').capitalize()
+
+print('Recife' in city)
